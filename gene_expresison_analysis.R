626286e5c6c3894dfe79214af785a2b524b7e3d3
MohammedYasserr <bmbmyasser@gmail.com>
2023-05-13 18:38:54 +0300

    Installing and laoding the followig packages: dplyr, tidyverse and GEOquery

---
+++ gene_expresison_analysis.R
@@ -0,0 +1,11 @@
+# Gene Expression data analysis....
+# setwd(E:\Current\Projects\gene_expresison_NCBI) 
+
+
+install.packages("dplyr")
+install.packages("tidyverse")
+install.packages("GEOquery")
+
+library(dplyr)
+library(tidyverse)
+library(GEOquery)
