d2737a7fc810ada45aeef8adfa5e1c11c16865f5
MohammedYasserr <bmbmyasser@gmail.com>
2023-05-13 20:53:43 +0300

    Importing and installing the "GEOquery" library from Bioconductors

--- gene_expresison_analysis.R
+++ gene_expresison_analysis.R
@@ -2,10 +2,19 @@
 # setwd(E:\Current\Projects\gene_expresison_NCBI) 
 
 
+# Installing the packages dplyr and tidyverse
 install.packages("dplyr")
 install.packages("tidyverse")
-install.packages("GEOquery")
 
+# Importing the libraries 
 library(dplyr)
 library(tidyverse)
+
+
+# Importing and installing the "GEOquery" library from Bioconductors
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("GEOquery")
+
 library(GEOquery)
